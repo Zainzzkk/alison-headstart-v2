f852d6fadc0f6859cf557cfab55157b0adba6f87
zainzzkk <51957827+Zainzzkk@users.noreply.github.com>
2022-08-05 22:24:39 +0100

    added routes for all pages and back to home

---
+++ src/templates/UploadDiplomaCodes/UploadDiplomaCodes.js
@@ -0,0 +1,22 @@
+import AlisonLogo from '../../assets/AlisonLogo.png';
+import HeadstartLogo from '../../assets/HeadstartLogo.png';
+
+import { useNavigate } from 'react-router-dom';
+
+
+function UploadDiplomaCodes() {
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <div>
+                <header class="image-align">
+                <img src={AlisonLogo} className="Alison-logo" alt="Alison Logo" onClick={() => navigate('/')}/>
+                <img src={HeadstartLogo} className="Headstart-logo" alt="Headstart Logo" onClick={() => navigate('/')}/>
+                </header>
+            </div>
+        </div>
+    )
+}
+
+export default UploadDiplomaCodes;
