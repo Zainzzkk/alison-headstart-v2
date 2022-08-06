6a7b8f0a53ada46a09c817fb7e9829e2f363739a
zainzzkk <51957827+Zainzzkk@users.noreply.github.com>
2022-08-06 22:45:47 +0100

    added course upload page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './templates/MainPage/MainPage';
 import UploadCourseCodes from './templates/UploadCourseCodes/UploadCourseCodes';
@@ -11,22 +12,22 @@ import UploadDiplomaCodes from './templates/UploadDiplomaCodes/UploadDiplomaCode
 import UploadCoursesCatalogue from './templates/UploadCoursesCatalogue/UploadCoursesCatalogue';
 
 function App() {
-    return (  
-       <div className="App"> 
-            <Routes>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/upload-course-codes" element={<UploadCourseCodes />} /> 
-                <Route path="/upload-diploma-codes" element={<UploadDiplomaCodes />} /> 
-                <Route path="/upload-courses-catalogue" element={<UploadCoursesCatalogue />} /> 
-                <Route path="/review-courses" element={<ReviewCourses />} /> 
-                <Route path="/review-learners" element={<ReviewLearners />} /> 
-                <Route path="/review-progress" element={<ReviewProgress />} /> 
-                <Route path="/update-completion" element={<UpdateCompletion />} /> 
-                <Route path="/update-learners" element={<UpdateLearners />} /> 
-                <Route path="/update-learners-info" element={<UpdateLearnersInfo />} /> 
-            </Routes>
-        </div>
-    );
-  }
-  
-  export default App;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/upload-course-codes" element={<UploadCourseCodes />} />
+        <Route path="/upload-diploma-codes" element={<UploadDiplomaCodes />} />
+        <Route path="/upload-courses-catalogue" element={<UploadCoursesCatalogue />} />
+        <Route path="/review-courses" element={<ReviewCourses />} />
+        <Route path="/review-learners" element={<ReviewLearners />} />
+        <Route path="/review-progress" element={<ReviewProgress />} />
+        <Route path="/update-completion" element={<UpdateCompletion />} />
+        <Route path="/update-learners" element={<UpdateLearners />} />
+        <Route path="/update-learners-info" element={<UpdateLearnersInfo />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
