499ea16cd3c9d1ece3662469b9638f6f584fd836
zainzzkk <51957827+Zainzzkk@users.noreply.github.com>
2022-08-11 22:19:37 +0100

    added postgres to script

--- courses-api/config/database.js
+++ courses-api/config/database.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const dotenv = require('dotenv');
 dotenv.config();
 
-module.exports = new Sequelize(`postgres://${process.env.API_USERNAME}:${process.env.API_PASSWORD}@127.0.0.1:5434/HeadstartTest2`, {
+module.exports = new Sequelize(`postgres://${process.env.API_USERNAME}:${process.env.API_PASSWORD}@localhost:5434/HeadstartTest2`, {
   define: {
     timestamps: false,
   },
