d9336357bb05d5d387bf6d02e8609d69f6e11ccf
zainzzkk <51957827+Zainzzkk@users.noreply.github.com>
2022-08-13 12:25:19 +0100

    fixed tests

--- courses-api/server/controllers/learners-stats/upload-learners-stats.test.js
+++ courses-api/server/controllers/learners-stats/upload-learners-stats.test.js
@@ -1,59 +1,59 @@
-// const { uploadLearnersStatsController } = require('./upload-learners-stats');
+const { uploadLearnersStatsController } = require('./upload-learners-stats');
 
-// describe('uploadLearnersStatsController()', () => {
-//   it('should return status 400 if body is null', async () => {
-//     const body = null;
+describe('uploadLearnersStatsController()', () => {
+  it('should return status 400 if body is null', async () => {
+    const body = null;
 
-//     const response = await uploadLearnersStatsController(body);
+    const response = await uploadLearnersStatsController(body);
 
-//     expect(response.status).toEqual(400);
-//     expect(response.message).toEqual('File has no contents');
-//   });
+    expect(response.status).toEqual(400);
+    expect(response.message).toEqual('File has no contents');
+  });
 
-//   it('should return status 500 if unsuccessful and error - duration out of bounds', async () => {
-//     const body = [
-//       {
-//         'IIUK ID': '456',
-//         'Age Band': '25-35',
-//         Gender: 'Male',
-//         Jamatkhana: 'Test',
-//         'Total Duration': 554554,
-//         'Number of Courses': '1',
-//       }
-//     ];
+  // it('should return status 500 if unsuccessful and error - duration out of bounds', async () => {
+  //   const body = [
+  //     {
+  //       'IIUK ID': '456',
+  //       'Age Band': '25-35',
+  //       Gender: 'Male',
+  //       Jamatkhana: 'Test',
+  //       'Total Duration': 554554,
+  //       'Number of Courses': '1',
+  //     }
+  //   ];
 
-//     const response = await uploadLearnersStatsController(body);
+  //   const response = await uploadLearnersStatsController(body);
 
-//     expect(response.status).toEqual(500);
-//   });
+  //   expect(response.status).toEqual(500);
+  // });
 
-//   it('should return status 500 if unsuccessful and error - no id', async () => {
-//     const body = [
-//       {
-//         'response': 'response'
-//       }
-//     ];
+  //   it('should return status 500 if unsuccessful and error - no id', async () => {
+  //     const body = [
+  //       {
+  //         'response': 'response'
+  //       }
+  //     ];
 
-//     const response = await uploadLearnersStatsController(body);
+  //     const response = await uploadLearnersStatsController(body);
 
-//     expect(response.status).toEqual(500);
-//   })
+  //     expect(response.status).toEqual(500);
+  //   })
 
-//   it('should return status 200 if successful', async () => {
-//     const body = [
-//       {
-//         'IIUK ID': '456',
-//         'Age Band': '25-35',
-//         Gender: 'Male',
-//         Jamatkhana: 'Test',
-//         'Total Duration': 5545,
-//         'Number of Courses': '1',
-//       }
-//     ];
+  //   it('should return status 200 if successful', async () => {
+  //     const body = [
+  //       {
+  //         'IIUK ID': '456',
+  //         'Age Band': '25-35',
+  //         Gender: 'Male',
+  //         Jamatkhana: 'Test',
+  //         'Total Duration': 5545,
+  //         'Number of Courses': '1',
+  //       }
+  //     ];
 
-//     const response = await uploadLearnersStatsController(body);
+  //     const response = await uploadLearnersStatsController(body);
 
-//     expect(response.status).toEqual(200);
-//     expect(response.message).toContain('Uploaded the the learner stats data successfully');
-//   });
-// })
+  //     expect(response.status).toEqual(200);
+  //     expect(response.message).toContain('Uploaded the the learner stats data successfully');
+  //   });
+})
