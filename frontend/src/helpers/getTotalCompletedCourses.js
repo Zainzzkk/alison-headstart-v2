ca8a15d9369ec758d3375ea24f819f6b45786b5a
zainZzKk <51957827+Zainzzkk@users.noreply.github.com>
2022-10-12 15:31:26 +0530

    feat(review-progress): Overall Stats (#53)

--- frontend/src/helpers/getTotalCompletedCourses.js
+++ frontend/src/helpers/getTotalCompletedCourses.js
@@ -2,6 +2,10 @@
 export default function getTotalCompletedCourses(data) {
   // if completion is 100 then adds 1 to counter
   const count = data.reduce((counter, course) => course.Completion === 100 ? counter += 1 : counter, 0); // eslint-disable-line
+  const percentage = (count / data.length) * 100;
 
-  return count;
+  return {
+    count,
+    percentage: percentage.toFixed(1),
+  };
 }
